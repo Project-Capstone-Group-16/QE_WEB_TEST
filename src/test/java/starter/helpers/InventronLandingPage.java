dffdd9baaf1fd05c0053929c246eaacf9b79accc
Arul Budi Kalimat <97519455+RsTi3@users.noreply.github.com>
2023-05-31 23:22:18 +0700

    (update) update project on folder src
    add some codes to project

--- src/test/java/starter/helpers/InventronLandingPage.java
+++ src/test/java/starter/helpers/InventronLandingPage.java
@@ -1,11 +1,15 @@
 package starter.helpers;
 
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.targets.Target;
 import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.By;
 
-@DefaultUrl("https://inventron-indonesia.netlify.app/")
+@DefaultUrl("https://inventron-indonesia.netlify.app")
 public class InventronLandingPage extends PageObject {
 
+    public static Target BUTTON_EKSPLORASI = Target.the("button eksplorasi").located(By.xpath("//a[contains(text(),'Eksplorasi')]"));
 
+    public static Target BUTTON_MULAI = Target.the("button mulai").located(By.xpath("//button[contains(text(),'Mulai')]"));
 
 }
